3f146a3b483b22d2b8354a83655f4c65561266d3
shimyounghyun <lhlrol1004@naver.com>
2020-07-26 03:01:12 +0900

    delete build folder

--- build/utils/RoutingConfig.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.routingControllerOptions = {
-    cors: true,
-    routePrefix: "/api",
-    controllers: [__dirname + "/../controllers/*{.ts,.js}"],
-    middlewares: [__dirname + "/../middlewares/*{.ts,.js}"],
-};
