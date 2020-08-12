784e18c97ed8956f6a969ded46103b63f1789b4b
shimyounghyun <lhlrol1004@naver.com>
2020-08-12 23:18:52 +0900

    auth 미들웨어 401 함수, 테스트 수정

--- src/middlewares/AuthMiddleware.ts
+++ src/middlewares/AuthMiddleware.ts
@@ -8,7 +8,7 @@ export class LocalAuthenticate implements ExpressMiddlewareInterface {
     use(req: any, res: any, next: (err?: any) => any): any {
        return this.authenticate( (err, user, info) => {
            const error = info || err;
-           if (error) return res.json(401, error);
+           if (error) return res.status(401).json(error);
            req.user = user;
            next();
        })(req, res, next);
