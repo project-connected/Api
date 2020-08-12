1557d6db3b8d089e8720bf5c4ef351268dcc6f0b
shimyounghyun <lhlrol1004@naver.com>
2020-08-12 14:20:15 +0900

    Common 기능 추가

---
+++ src/dtos/EnumSkill.ts
@@ -0,0 +1,17 @@
+export enum Skill {
+    NODE = 'Node.JS',
+    PHOTHOSHOP = 'Photoshop',
+    REACT = 'React.JS',
+    SWIFT = 'Swift',
+    JAVASCRIPT = 'JavaScript',
+    C = 'C',
+    CSHARP = 'C#',
+    CPLUS = 'C++',
+    JAVA = 'Java',
+    Go = 'Go',
+    R = 'R',
+    PYTHON = 'Python',
+    SPRING = 'Spring',
+    PHP = 'PHP',
+    ANDROID = 'Android'
+}
---
+++ src/dtos/EnumStatus.ts
@@ -0,0 +1,6 @@
+export enum Status {
+    WAITING = '대기중',
+    OWNER = '팀장',
+    CREW = '팀원',
+    NONE = '신청 가능'
+}
