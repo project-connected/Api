d42c932d3d80736742fb0daaaa20fcdac8f3c00d
progresshans <progresshans@gmail.com>
2020-08-15 20:48:30 +0900

    user test 추가

--- tests/api/user.test.ts
+++ tests/api/user.test.ts
@@ -15,20 +15,31 @@ afterAll(async () => {
 })
 
 describe("POST /api/user", ()=> {
-    it("200: 유저 생성 성공", async () => {
-        const testUser = Builder(CreateUserDto)
-            .userName('test')
-            .password("1234")
-            .email('test@test.com')
-            .build();
+    const testUser = Builder(CreateUserDto)
+        .userName('test')
+        .password("1234")
+        .email('test@test.com')
+        .build();
 
+    it("200: 유저 생성 성공", async () => {
         const response = await request(app)
             .post("/api/user")
             .send(testUser)
-            .expect(200);
 
         const {body} = response;
+        expect(body.status).toEqual(200)
         expect(body.result.userName).toEqual("test");
         expect(body.result.email).toEqual('test@test.com');
     });
+
+    it("409: 중복 유저, 유저 생성 실패", async () => {
+        const response = await request(app)
+            .post("/api/user")
+            .send(testUser)
+
+        const {body} = response
+        expect(body.status).toEqual(409)
+        expect(body.result.userName).toEqual("test");
+        expect(body.result.email).toEqual('test@test.com');
+    })
 });
